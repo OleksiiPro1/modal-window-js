59539104109b7f3f336c4cad7a24e55c7613ed19
OleksiiPro1 <ukr.top@ukr.net>
2023-04-17 12:04:29 +0200

    plus btn

--- script.js
+++ script.js
@@ -4,28 +4,38 @@ const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.close-modal-window');
 const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
-console.log(btnsShowModalWindow);
+// console.log(btnsShowModalWindow);
+const addHiddenClass = function() {
+    modalWindow.classList.add('hidden');       
+    overlay.classList.add('hidden');
+}
 
 for (let i = 0; i < btnsShowModalWindow.length; i++) {
     btnsShowModalWindow[i].addEventListener('click', function(){
-        console.log('click');
+        // console.log('click');
         modalWindow.classList.remove('hidden');
         // modalWindow.style.display = 'block';
         overlay.classList.remove('hidden');
 
     })
 
-    const addHiddenClass = function() {
-        modalWindow.classList.add('hidden');       
-        overlay.classList.add('hidden');
-    }
+
 
     btnCloseModalWindow.addEventListener('click', addHiddenClass );
 
     overlay.addEventListener('click', addHiddenClass );
+
+
 }
 
-
+document.addEventListener('keydown', function(e) {
+   
+    if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+        
+            addHiddenClass();
+        
+    }
+} ) 
 
 
 
