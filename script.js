62258655a98a4700eb76ec793e0ce678a32577ea
OleksiiPro1 <ukr.top@ukr.net>
2023-04-17 11:37:28 +0200

    finish

--- script.js
+++ script.js
@@ -10,7 +10,19 @@ for (let i = 0; i < btnsShowModalWindow.length; i++) {
     btnsShowModalWindow[i].addEventListener('click', function(){
         console.log('click');
         modalWindow.classList.remove('hidden');
+        // modalWindow.style.display = 'block';
+        overlay.classList.remove('hidden');
+
     })
+
+    const addHiddenClass = function() {
+        modalWindow.classList.add('hidden');       
+        overlay.classList.add('hidden');
+    }
+
+    btnCloseModalWindow.addEventListener('click', addHiddenClass );
+
+    overlay.addEventListener('click', addHiddenClass );
 }
 
 
