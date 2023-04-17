4df0316dd483f60e318bf6a522101739823a65ee
OleksiiPro1 <ukr.top@ukr.net>
2023-04-17 11:06:54 +0200

    for and classes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 'use strict';
 
-const modalWindow = 
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+console.log(btnsShowModalWindow);
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+    console.log(btnsShowModalWindow[i].textContent);
+}
+
+
+
+
+
+
